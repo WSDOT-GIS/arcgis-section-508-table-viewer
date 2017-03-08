09e71151159658a283c6afe906c2e2ffdbedbc29
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-03-07 16:20:28 -0800

    Added compiled JS files.

--- script/index.js
+++ script/index.js
@@ -1,3 +1,38 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
@@ -38,18 +73,77 @@
     // Get the specified URL from the search string.
     var pageUrl = new URL(location.href);
     // searchParams property defined incorrectly as searchparams.
+    // This issue is scheduled to be fixed at TypeScript 2.3,
+    // at which time this workaround will no longer be necessary.
+    // let url = pageUrl.searchParams.get("url");
     var url = pageUrl.searchParams.get("url");
+    /**
+     * Gets information about a map service.
+     * @param serviceUrl URL to the map service layer.
+     */
+    function getServiceInfo(serviceUrl) {
+        return __awaiter(this, void 0, void 0, function () {
+            var response, json;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch(serviceUrl + "?f=json")];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 2:
+                        json = _a.sent();
+                        if (json.error) {
+                            throw json.error;
+                        }
+                        return [2 /*return*/, json];
+                }
+            });
+        });
+    }
+    function addTable(serviceUrl) {
+        return __awaiter(this, void 0, void 0, function () {
+            var table, error_1, errorMsg;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        _a.trys.push([0, 2, , 3]);
+                        return [4 /*yield*/, tableUtils_1.default(new URL(serviceUrl))];
+                    case 1:
+                        table = _a.sent();
+                        document.body.appendChild(table);
+                        return [2 /*return*/, table];
+                    case 2:
+                        error_1 = _a.sent();
+                        errorMsg = document.createElement("p");
+                        errorMsg.textContent = "Error loading data from " + serviceUrl;
+                        document.body.appendChild(errorMsg);
+                        return [3 /*break*/, 3];
+                    case 3: return [2 /*return*/];
+                }
+            });
+        });
+    }
     // If the url is provided in the search, load the data from the specified service.
     // Otherwise display a form the user can use to specify a service URL.
     if (url) {
-        var promise = tableUtils_1.default(new URL(url));
-        promise.then(function (frag) {
-            document.body.appendChild(frag);
-        });
-        promise.catch(function (error) {
-            var errorMsg = document.createElement("p");
-            errorMsg.textContent = "Error loading data from " + url;
-            document.body.appendChild(errorMsg);
+        var tablePromise = addTable(url);
+        var layerInfoPromise = getServiceInfo(url);
+        var allPromise = Promise.all([tablePromise, layerInfoPromise]);
+        allPromise.then(function (results) {
+            var table = results[0];
+            var data = results[1];
+            // Add title to page.
+            if (data.name) {
+                document.title = data.name;
+                var header = document.createElement("h1");
+                header.textContent = data.name;
+                document.body.insertBefore(header, document.body.firstChild);
+                if (table) {
+                    var caption = document.createElement("caption");
+                    caption.textContent = data.name;
+                    table.appendChild(caption);
+                }
+            }
         });
     }
     else {
--- script/tableUtils.js
+++ script/tableUtils.js
@@ -1,3 +1,38 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 (function (factory) {
     if (typeof module === "object" && typeof module.exports === "object") {
         var v = factory(require, exports);
@@ -14,22 +49,31 @@
      * @param layerUrl - Feature Layer URL.
      */
     function getData(layerUrl) {
-        var sp = new URLSearchParams();
-        sp.append("where", "1=1");
-        sp.append("outFields", "*");
-        sp.append("returnGeometry", false.toString());
-        sp.append("f", "json");
-        var url = new URL(layerUrl + "/query?" + sp.toString());
-        return fetch(url.toString()).then(function (response) {
-            return response.json();
-        }).then(function (jsonResponse) {
-            var err = jsonResponse;
-            if (err.error) {
-                throw err.error;
-            }
-            return jsonResponse;
-        }, function (reason) {
-            throw reason;
+        return __awaiter(this, void 0, void 0, function () {
+            var sp, url, response, json, err;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0:
+                        sp = new URLSearchParams();
+                        sp.append("where", "1=1");
+                        // TODO: Get fields form service info, pass in that array minus OBJECTID field.
+                        sp.append("outFields", "*");
+                        sp.append("returnGeometry", false.toString());
+                        sp.append("f", "json");
+                        url = new URL(layerUrl + "/query?" + sp.toString());
+                        return [4 /*yield*/, fetch(url.toString())];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.json()];
+                    case 2:
+                        json = _a.sent();
+                        err = json;
+                        if (err.error) {
+                            throw err.error;
+                        }
+                        return [2 /*return*/, json];
+                }
+            });
         });
     }
     exports.getData = getData;
@@ -38,17 +82,20 @@
      * @param featureSet - A feature set.
      */
     function createTableFromData(featureSet) {
-        var frag = document.createDocumentFragment();
         var table = document.createElement("table");
-        frag.appendChild(table);
         var tbody = document.createElement("tbody");
         table.appendChild(tbody);
         var thead = document.createElement("thead");
         table.appendChild(thead);
         var row = document.createElement("tr");
         thead.appendChild(row);
+        // Omit the Object ID field.
+        var oidFieldNameRe = /^O(bject)ID$/i;
         for (var _i = 0, _a = featureSet.fields; _i < _a.length; _i++) {
             var field = _a[_i];
+            if (oidFieldNameRe.test(field.name)) {
+                continue;
+            }
             var th = document.createElement("th");
             th.textContent = field.alias || field.name;
             row.appendChild(th);
@@ -59,6 +106,9 @@
             row = document.createElement("tr");
             for (var _d = 0, _e = featureSet.fields; _d < _e.length; _d++) {
                 var field = _e[_d];
+                if (oidFieldNameRe.test(field.name)) {
+                    continue;
+                }
                 var cell = document.createElement("td");
                 var value = feature.attributes[field.name];
                 if (dateRe.test(field.type) && typeof value === "number") {
@@ -66,8 +116,8 @@
                     // Add a <time> element with the date.
                     var theDate = new Date(value);
                     var time = document.createElement("time");
-                    time.dateTime = theDate.toISOString();
-                    time.textContent = "" + theDate;
+                    time.setAttribute("dateTime", theDate.toISOString());
+                    time.textContent = "" + theDate.toLocaleString();
                     cell.appendChild(time);
                 }
                 else {
@@ -78,7 +128,7 @@
             }
             tbody.appendChild(row);
         }
-        return frag;
+        return table;
     }
     exports.createTableFromData = createTableFromData;
     /**
@@ -86,9 +136,16 @@
      * @param layerUrl URL to a feature layer.
      */
     function default_1(layerUrl) {
-        var dataPromise = getData(layerUrl);
-        return dataPromise.then(function (featureSet) {
-            return createTableFromData(featureSet);
+        return __awaiter(this, void 0, void 0, function () {
+            var featureSet;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, getData(layerUrl)];
+                    case 1:
+                        featureSet = _a.sent();
+                        return [2 /*return*/, createTableFromData(featureSet)];
+                }
+            });
         });
     }
     exports.default = default_1;

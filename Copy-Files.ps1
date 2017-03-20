5572b5f5190b85c2f672e9736a40ef2c881d2f4b
Jeff Jacobson <JeffJacobson@users.noreply.github.com>
2017-03-20 10:14:45 -0700

    Added comments to code files.

--- Copy-Files.ps1
+++ Copy-Files.ps1
@@ -1,3 +1,8 @@
+<#
+.SYNOPSIS
+    Copies script files from node_modules into script directory.
+#>
+
 $files = $(".\node_modules\requirejs\require.js", ".\node_modules\whatwg-fetch\fetch.js", ".\node_modules\babel-polyfill\dist\polyfill.min.js")
 
 foreach ($file in $files) {
--- src/worker/arcgisWorker.ts
+++ src/worker/arcgisWorker.ts
@@ -34,10 +34,6 @@ async function getServiceInfo(serviceUrl: string) {
  * If omitted, all fields ("*") will be returned.
  */
 async function getData(layerUrl: string, fieldNames?: string[]) {
-    // Get the names of the fields, excluding the OID field.
-    // tslint:disable-next-line:max-line-length
-    // let fieldNames = serviceInfo.fields.filter((field) => field.type !== "esriFieldTypeOID").map((field) => field.name);
-
     let sp: any = {
         f: "json",
         outFields: fieldNames ? fieldNames.join(",") : "*",
@@ -53,30 +49,44 @@ async function getData(layerUrl: string, fieldNames?: string[]) {
         searchParts.push(`${key}=${encodeURIComponent(sp[key])}`);
     }
 
+    // Query the service to get all data (or the max amount of records allowed by the server).
     let url = `${layerUrl}/query?${searchParts.join("&")}`;
     let response = await fetch(url);
+    // Parse the JSON text response.
     let json = await response.json() as IFeatureSet | IError;
+    // Sometimes "successful" HTTP requests from ArcGIS server are still errors.
+    // Throw an exception if an "error" property is present in the returned JSON.
     let err = json as IError;
     if (err.error) {
         throw err.error;
     }
+    // Return the FeatureSet if no errors were encountered.
     let featureSet = json as IFeatureSet;
     return featureSet;
 }
 
+// Setup handling messages from the page.
 addEventListener("message", async (msgEvt) => {
+    // The only messages set from page to worker will be the layer URL string.
     if (msgEvt.data && typeof msgEvt.data === "string") {
         let url = msgEvt.data;
+        // Get info about the map / feature service layer.
         let svcPromise = getServiceInfo(url);
         let serviceInfo = await svcPromise;
+        // Create a list of fields that excludes OID and geometry.
         let fields = serviceInfo.fields.filter(
             (field) => field.type !== "esriFieldTypeOID" && field.type !== "esriFieldTypeGeometry");
+        // Create an array of field names (from the filtered list).
         let fieldNames = fields.map((field) => field.name);
+        // Send layer info back to the page so it can create table and column headers
+        // while awaiting features.
         postMessage({
             fields,
             serviceInfo,
             type: "serviceInfo"
         });
+        // Query the service to get features (up to the max amount set by the service).
+        // When the query is complete, the features will be sent to the page.
         let dataPromise = getData(url, fieldNames);
         dataPromise.then((featureSet) => {
             postMessage({
@@ -85,6 +95,9 @@ addEventListener("message", async (msgEvt) => {
             });
         });
 
+        // Once all of the HTTP queries to the feature service have
+        // been completed, the worker is no longer needed and can be
+        // closed.
         let allPromise = Promise.all([svcPromise, dataPromise]);
         allPromise.then((results) => {
             postMessage({
